e94144e1a0f00de7a2024346cca94b38a8f53742
ANTC HUB <aapis3308@gmail.com>
2026-04-04 04:17:17 +0800

    Add src/config.js

---
+++ src/config.js
@@ -0,0 +1,17 @@
+const config = {
+    HARGA_PER_M: 2000,
+    NAMA_BISNIS: "CDID BY Vireon Interactive",
+    FOUNDER: "Apip",
+    DISCORD_WEBHOOK: "https://discord.com/api/webhooks/1475185474476834999/nOPtFreLMZ0ysp82oLDqsuftzdgmUcAHoqqfwqmPmv2kkhDijKmIp753B2WG-j2nWen8",
+    AUTO_REPLY_MAX: 3,
+    AUTO_REPLY_MSG: `Hallo, saya asisten agen dari Vireon Interactive 🤖
+Saya bertugas untuk menangani customer dan juga report dari customer.
+Saya siap melayani anda!
+
+Saya bertugas jika founder/Apip sedang dalam mode istirahat atau tidur.
+Kami siap melayani anda 24 jam!
+
+Ketik *.Menu* untuk melihat daftar layanan kami.`
+};
+
+export default config;
